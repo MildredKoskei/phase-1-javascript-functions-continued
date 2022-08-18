d53378085466081ceb50b65ee1286ada307d5e3b
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-18 10:18:45 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+
+ function mondayWork(work = "go to the office"){
+     return`This Monday, I will ${work}.`
+};
+
+mondayWork();
+mondayWork("work from home");
+
+
+function wrapAdjective(wrap = "*"){
+return function(adjective= "a hard worker"){
+    return `You are ${wrap}${adjective}${wrap}!`
+};
+};
+wrapAdjective()
